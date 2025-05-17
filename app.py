02734ac786e29414e60bf59c9ae89530dfb98ca5
Poorvin123HR <hrpoorvin@gmail.com>
2025-05-17 15:09:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Streamlit chatbot")
 import os
 os.environ["GOOGLE_API_KEY"]=st.secrets["GOOGLE_API_KEY"]
-import streamlit as st
+
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
